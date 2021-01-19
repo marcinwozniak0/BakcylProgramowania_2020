ac94f1dd7ad7206f0775ebdba914acb7478fbf25
Kgebski <kgebski68@gmail.com>
2021-01-19 16:16:48 +0100

    Default window created

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
-int main()
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
 {
-   return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
