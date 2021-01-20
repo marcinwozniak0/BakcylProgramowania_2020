c7e206f926d00847ec4cdd82780f6b878bc2dc71
Wiktor Szewczyk <wiktorszewczyk21@interia.pl>
2021-01-20 22:55:48 +0100

    saving logs to seperate files with date

--- Logger/Logger.cpp
+++ Logger/Logger.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <time.h>
 
 #include "Logger.hpp"
 
+const std::string currentDateTime() 
+{
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       currentTime[80];
+    tstruct = *localtime(&now);
+    strftime(currentTime, sizeof(currentTime), "%Y-%m-%d_%X", &tstruct);
+
+    return currentTime;
+}
+
 void Logger::log(const std::string& msg)
 {
-    if (logs.is_open())
-    {     
-        logs << msg << "\n";
-    }
-    else
-    {
-        std::ofstream outfile("logs.txt");
-
-        logs.open("logs.txt", std::ios::app);
-        
-        logs << msg << "\n";
-    }
+    std::string fileName = "logs_" + currentDateTime() + ".txt";
     
+    std::ofstream outfile(fileName);
+
+    logs.open(fileName, std::ios::app);
+        
+    logs << msg << "\n";
 
     std::cout << msg << std::endl;
 }
 
-Logger::~Logger() {
-logs.close();
+Logger::~Logger() 
+{
+    logs.close();
 }
 
