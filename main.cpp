11c50996c6edd0056b0946c77e6b1fb2a97cd5a1
gizlu <gizlu@gizlu.pl>
2021-01-22 18:13:36 +0100

    Add sqlite3 and jsoncpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <sqlite3.h>
+#include <json/value.h>
 
 int main(int argc, char *argv[])
 {
+    sqlite3* _db;
+    sqlite3_open(":memory:", &_db);
+    Json::Value root;
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
