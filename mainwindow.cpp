e5275127eb6a9caa15946b9b459dbdcf5b18f07f
Kgebski <kgebski68@gmail.com>
2021-02-17 21:34:45 +0100

    Finish a basic structure I crated a three basic boxes (card,search,decks) and blocked the option to change the size of the window By default, the window appears in the center of the screen.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,36 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+void CenterWindow(QWidget *widget);
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-}
 
+    this->setFixedSize(1200,700); //width, height in px
+
+    CenterWindow(this);
+
+}
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+void CenterWindow(QWidget *widget){
+    int x, y;
+    int screenWidth;
+    int screenHeight;
+
+    QDesktopWidget *desktop = QApplication::desktop();
+
+    screenWidth = desktop->width();
+    screenHeight = desktop->height();
+
+    x = (screenWidth - 1200) / 2;
+    y = (screenHeight - 700) / 2;
+
+    widget->setGeometry(x,y,1200,700);
+}
