cfd79ebd3b7a1ff790075b708940b6e6d7dbf87e
ankiedos <antek_kiedos@outlook.com>
2021-02-23 09:46:23 +0100

    Changed installation from APT to universal curl, because when I ask my friend which has non Debian-derived Linux to test this script he can't install dependencies, because he doesn't have APT.

--- build.bash
+++ build.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
 if [[ ! -f /usr/include/sqlite3.h ]]
 then
-    sudo apt-get install libsqlite3-dev
+    curl https://sqlite.org/2021/sqlite-autoconf-3340100.tar.gz -o "/usr/include/sqlite.tar.gz"
 fi
 if [[ ! -d /usr/include/jsoncpp/json ]]
 then
-    sudo apt-get install libjsoncpp-dev
+    curl https://raw.githubusercontent.com/open-source-parsers/jsoncpp/1.9.4/include/json/{allocator.h, value.h, assertions.h, config.h, forwards.h, json.h, json_features.h, reader.h, version.h, writer.h} -o /usr/include/jsoncpp/json/#1
+    # curl https://raw.githubusercontent.com/open-source-parsers/jsoncpp/1.9.4/src/lib_json/json_tool.h -o /usr/include/jsoncpp/json/json_value.h
+    # curl https://raw.githubusercontent.com/open-source-parsers/jsoncpp/1.9.4/src/lib_json/json_reader.cpp -o /usr/include/jsoncpp/json/json_reader.cpp
+    # curl https://raw.githubusercontent.com/open-source-parsers/jsoncpp/1.9.4/src/lib_json/json_writer.cpp -o /usr/include/jsoncpp/json/json_writer.cpp
+    # curl https://raw.githubusercontent.com/open-source-parsers/jsoncpp/1.9.4/src/lib_json/json_value.cpp
 fi
 if [[ ! -d /usr/lib/qt5/bin ]]
 then
