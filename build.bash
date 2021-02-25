ff28c0c82a2562cf59ae61b1e7ebb71d465f80d8
ankiedos <antek_kiedos@outlook.com>
2021-02-25 12:35:27 +0100

    Added creating new directories.

--- build.bash
+++ build.bash
@@ -6,7 +6,13 @@ then
 fi
 if [[ ! -d /usr/include/jsoncpp/json ]]
 then
-    curl https://raw.githubusercontent.com/open-source-parsers/jsoncpp/1.9.4/include/json/{allocator.h, value.h, assertions.h, config.h, forwards.h, json.h, json_features.h, reader.h, version.h, writer.h} -o /usr/include/jsoncpp/json/#1
+    CURRENTDIR=pwd
+    cd /usr/include/
+    mkdir jsoncpp
+    cd jsoncpp
+    mkdir json
+    curl https://raw.githubusercontent.com/open-source-parsers/jsoncpp/1.9.4/include/json/{allocator.h, value.h, assertions.h, config.h, forwards.h, json.h, json_features.h, reader.h, version.h, writer.h} -o #1
+    cd CURRENTDIR
 fi
 if [[ ! -d /usr/lib/qt5/bin ]]
 then
