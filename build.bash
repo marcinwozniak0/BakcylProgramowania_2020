7ca049a5b38df91d7f8f37f500eb8d6442bc0005
ankiedos <antek_kiedos@outlook.com>
2021-02-25 13:47:09 +0100

    Added CURRENTDIR variable to come back to working directory.

--- build.bash
+++ build.bash
@@ -1,20 +1,24 @@
 #!/bin/bash
+CURRENTDIR=pwd
 if [[ ! -f /usr/include/sqlite3.h ]]
 then
-    curl https://sqlite.org/2021/sqlite-autoconf-3340100.tar.gz -o "/usr/include/sqlite.tar.gz"
-    tar -zxvf /usr/include/sqlite.tar.gz /usr/include/sqlite/
+    cd /usr/include
+    touch sqlite.tar.gz
+    mkdir sqlite
+    curl https://sqlite.org/2021/sqlite-autoconf-3340100.tar.gz -o sqlite.tar.gz
+    tar -zxvf sqlite.tar.gz sqlite
 fi
 if [[ ! -d /usr/include/jsoncpp/json ]]
 then
-    CURRENTDIR=pwd
     cd /usr/include/
     mkdir jsoncpp
     cd jsoncpp
     mkdir json
     curl https://raw.githubusercontent.com/open-source-parsers/jsoncpp/1.9.4/include/json/{allocator.h, value.h, assertions.h, config.h, forwards.h, json.h, json_features.h, reader.h, version.h, writer.h} -o #1
-    cd CURRENTDIR
 fi
 if [[ ! -d /usr/lib/qt5/bin ]]
 then
     sudo apt-get install qt5-default
 fi
+
+cd CURRENTDIR
