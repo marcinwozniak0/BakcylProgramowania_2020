8c058140b2657cc5bc5d3421f994084815f0a64f
Wiktor Szewczyk <wiktorszewczyk21@interia.pl>
2021-03-04 21:34:29 +0100

    Improved DeckBuilder class

--- DeckBuilder.hpp
+++ DeckBuilder.hpp
@@ -3,6 +3,12 @@
 
 class DeckBuilder 
 {
+    int maxNumberOfCards;
+    int maxNumberOfHeroes;
+    int maxNumberOfEachCard;
+    std::string firstRegion;
+    std::string secondRegion;
+    
     void addCards(std::vector <std::string> cardsToAdd); //or <int> if we use ID
     void removeCards(std::vector <std::string> cardsToRemove); //or <int> if we use ID
     void deckValidator(std::vector <std::string> deck); //or <int> if we use ID
