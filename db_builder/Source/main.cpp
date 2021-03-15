a100b3942d222de61d665cc3f46b0c9348121986
gizlu <gizlu@gizlu.pl>
2021-03-15 23:51:55 +0100

    Add parsing of remaining tables
    Added parsing of "associatedCards", "cardSubtypes", "cardKeywords", "cardAssets"
    
    Db builder is kinda complete

--- db_builder/Source/main.cpp
+++ db_builder/Source/main.cpp
@@ -10,20 +10,24 @@ void fillGlobals(unique_sqlite3& db, const Json::Value& json);
 void fillCards(unique_sqlite3& db, const Json::Value& json);
 void createTables(unique_sqlite3& db);
 std::string getJsonMemberNameWithoutNuls(Json::ValueIteratorBase it);
+void fillSet(unique_sqlite3& db, const Json::Value& set);
+void fillAssoc(unique_sqlite3& db, const Json::Value& cards, const std::string& table_name, const std::string& arrName);
+void fillAssets(unique_sqlite3& db, const Json::Value& cards);
 
 bool is_empty(std::ifstream& pFile)
 {
     return pFile.peek() == std::ifstream::traits_type::eof();
 }
 
-Json::Value getJsonFromFile(const char filename[], const char download_url[])
+Json::Value getJsonFromFile(const std::string& filename, const std::string& download_url)
 {
     Json::Value json;
     std::ifstream file(filename);
     if (is_empty(file))
     {
         // ya ya. This sucks, but it is sufficient for this PoC
-        throw std::runtime_error(std::string("There are no json in globals-ru_ru.json. Download it from " + std::string(download_url)));
+        throw std::runtime_error(
+            std::string("There are no json in " + filename + " Download it from " + std::string(download_url)));
     }
     file >> json;
     return json;
@@ -35,11 +39,18 @@ int main()
     sqlite3_exec(db.get(), "BEGIN TRANSACTION;", NULL, NULL, NULL);
     createTables(db);
 
-    Json::Value json;
-    json = getJsonFromFile("globals-ru_ru.json", "https://dd.b.pvp.net/latest/core/ru_ru/data/globals-ru_ru.json");
-    fillGlobals(db, json);
-    json = getJsonFromFile("set1-ru_ru.json", "https://dd.b.pvp.net/latest/core/ru_ru/data/globals-ru_ru.json");
-    fillTableWithArrOfDicts(db, "cards", json); 
+    // temporary ugly shit. We are going to abadon it for sake of auto downloading
+    Json::Value globalsJson =
+        getJsonFromFile("globals-ru_ru.json", "https://dd.b.pvp.net/latest/core/ru_ru/data/globals-ru_ru.json");
+    fillGlobals(db, globalsJson);
+    for (char i = '1'; i < '5'; ++i)
+    {
+        const auto setName = std::string("set") + i;
+        const auto fileName = setName + "-ru_ru.json";
+        auto url = "https://dd.b.pvp.net/latest/" + setName + "/ru_ru/data/" + fileName;
+        Json::Value setJson = getJsonFromFile(fileName, url);
+        fillSet(db, setJson);
+    }
 
     sqlite3_exec(db.get(), "END TRANSACTION;", NULL, NULL, NULL);
 }
@@ -50,7 +61,46 @@ void fillGlobals(unique_sqlite3& db, const Json::Value& json)
     {
         auto tableName = getJsonMemberNameWithoutNuls(field);
         auto& value = *field;
-        fillTableWithArrOfDicts(db, tableName.c_str(), value); 
+        fillTableWithArrOfDicts(db, tableName.c_str(), value);
+    }
+}
+
+void fillSet(unique_sqlite3& db, const Json::Value& set)
+{
+    fillTableWithArrOfDicts(db, "cards", set);
+    fillAssoc(db, set, "cardSubtypes", "subtypes");
+    fillAssoc(db, set, "associatedCards", "associatedCardRefs");
+    fillAssoc(db, set, "cardKeywords", "keywordRefs");
+    fillAssets(db, set);
+}
+
+void fillAssoc(unique_sqlite3& db, const Json::Value& cards, const std::string& table_name, const std::string& arrName)
+{
+    sqlite3_stmt* stmt = prepareInsertStatement(db, table_name.c_str(), 2);
+    for (const auto& card : cards)
+    {
+        sqlite3_bind_text(stmt, 1, card["cardCode"].asCString(), -1, NULL);
+        for (const auto& elem : card[arrName])
+        {
+            sqlite3_bind_text(stmt, 2, elem.asCString(), -1, NULL);
+            execDumbStmt(db, stmt);
+        }
+    }
+    sqlite3_finalize(stmt);
+}
+
+void fillAssets(unique_sqlite3& db, const Json::Value& cards)
+{
+    sqlite3_stmt* stmt = prepareInsertStatement(db, "cardAssets", 3);
+    for (const auto& card : cards)
+    {
+        sqlite3_bind_text(stmt, 1, card["cardCode"].asCString(), -1, NULL);
+        for (const auto& asset : card["assets"])
+        {
+            sqlite3_bind_text(stmt, 2, asset["gameAbsolutePath"].asCString(), -1, NULL);
+            sqlite3_bind_text(stmt, 3, asset["fullAbsolutePath"].asCString(), -1, NULL);
+            execDumbStmt(db, stmt);
+        }
     }
 }
 
