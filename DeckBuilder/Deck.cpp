81076375e59f4520d7dc6afc37f19c68968ec90b
Wiktor Szewczyk <wiktorszewczyk21@interia.pl>
2021-03-16 16:00:41 +0100

    Created Deck.hpp and Deck.cpp, reorganized files

---
+++ DeckBuilder/Deck.cpp
@@ -0,0 +1,12 @@
+#include "Deck.hpp"
+
+Deck::Deck()
+{
+    int numberOfCards = 0;
+    int numberOfHeroes = 0;
+    std::string firstRegion = "";
+    std::string secondRegion = "";
+    std::vector <Card> deck;
+}
+
+Deck::~Deck() {}
