8cd534a76f8b68cbda9d23798224d2999275e1cb
dsopocko <dawidsopocko@interia.eu>
2021-03-16 17:32:24 +0100

    Changes in addCard method in DeckBuilder.cpp

--- DeckBuilder/DeckBuilder.hpp
+++ DeckBuilder/DeckBuilder.hpp
@@ -1,18 +1,22 @@
 #include <string>
 #include <vector>
+#include <map>
 #include "Card.hpp"
 #include "Deck.hpp"
-
+#include "../Logger/Logger.hpp"
+#include "../Logger/ExceptionLevels.cpp"
 class DeckBuilder 
 {
     private:
     int maxNumberOfCards;
     int maxNumberOfHeroes;
     int maxNumberOfEachCard;
-
+    std::map<Card,int> cardCount;
+    Deck deck;
     public:
     DeckBuilder();
     ~DeckBuilder();
     void addCard(Card &cardToAdd);
     void removeCard(Card &cardToRemove);
 }; 
+
