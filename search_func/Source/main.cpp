466fe04b3c1ea181b3d3b12b6ab81bab66506eb9
nskowron <nel.skowronek@gmail.com>
2021-03-28 21:36:32 +0200

    Modified the way data is turned into cards.

--- search_func/Source/main.cpp
+++ search_func/Source/main.cpp
@@ -2,9 +2,8 @@
 #include <sqlite3.h>
 #include "search_func.hpp"
 
+
 int main()
 {
-	std::string str = std::to_string(static_cast<std::size_t> (11));
-	std::cout << "ok " << str << std::endl;
 	return 0;
 }
--- search_func/Source/search_func.cpp
+++ search_func/Source/search_func.cpp
@@ -9,8 +9,10 @@ std::vector<Card> searchFor(SearchFlags sf)
 	sqlite3* db;
 	sqlite3_open("database.sql", &db);
 	std::string mainQuery = prepareSQLQuery(sf);
-	std::string fullQuery = "SELECT * FROM cards NATURAL JOIN ("
-		+ mainQuery + ");";
+	std::string fullQuery = "SELECT cardID.cardCode AS cardCode, regionRef, attack, cost, health, spellSpeedRef, rarityRef, "
+	    "collectible, [set], descriptionRaw, levelupDescriptionRaw, flavorText, [name], supertype, [type], gameAbsolutePath, "
+	    "fullAbsolutePath FROM (" + mainQuery + ") AS cardID NATURAL JOIN cards NATURAL JOIN cardAssets;";
+	    
 	int exit = sqlite3_exec(db, fullQuery.c_str(), fillResultArrayWCards, NULL, NULL);
 	if(exit != SQLITE_OK)
 	{
@@ -24,28 +26,48 @@ std::vector<Card> searchFor(SearchFlags sf)
 }
 
 
-Card turnIntoCard(char** data, std::vector<std::string> colNames)
+Card turnIntoCard(std::string* data)
 {
     Card card;
-	card.setCardCode(data[find(colNames, "cardCode")]);
-	//set rest of card
+	card.setCardCode( *data++ );
+    //card.setRegionRef( *data++ );
+    card.setAttack( std::stoi(*data++) );
+    card.setCost( std::stoi(*data++) );
+    card.setHealth( std::stoi(*data++) );
+    //card.setSpellSpeedRef( *data++ );
+    //card.setRarityRef( *data++ );
+    card.setCollectible( std::stoi(*data++) );
+    card.setSet( *data++ );
+    card.setDescriptionRaw( *data++ );
+    card.setLevelupDescriptionRaw( *data++ );
+    card.setFlavorText( *data++ );
+    card.setName( *data++ );
+    card.setSupertype( *data++ );
+    //card.setType( *data++ );
+    
+    std::map<std::string, std::string> assets;
+    assets["gameAbsolutePath"] = *data++;
+    assets["fullAbsolutePath"] = *data++;
+    card.setCardAssets( assets );
 	
 	return card;
 }
 
 
-int fillResultArrayWCards(void* unused, int colCount, char** data, char** _colNames)
+int fillResultArrayWCards(void* unused, int colCount, char** _data, char** colNames)
 {
-	std::vector<std::string> colNames(colCount);
+    std::string* data = new std::string[colCount];
 	for(int i = 0; i < colCount; ++i)
 	{
-		std::string str(_colNames[i]);
-		colNames.at(i) = str;
+	    std::string str(_data[i]);
+		data[i] = str;
 	}
 
-	Card card = turnIntoCard(data, colNames);
+	Card card = turnIntoCard(data);
 	
 	results.push_back(card);
+	
+	delete (data - colCount);   //won't work until fields are commented
 	
 	return 0;
 }
