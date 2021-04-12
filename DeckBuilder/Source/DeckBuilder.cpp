89aae1cbdac375d732f3b60ed63184a7b4030e27
Piotr Skrzyński <76049506+piotrskrzynski1@users.noreply.github.com>
2021-04-12 16:19:37 +0200

    Update DeckBuilder.cpp

--- DeckBuilder/Source/DeckBuilder.cpp
+++ DeckBuilder/Source/DeckBuilder.cpp
@@ -1,19 +1,20 @@
 #include "DeckBuilder.hpp"
+#include "Deck.hpp"
 
 DeckBuilder::DeckBuilder()
 {
     int maxNumberOfCards = 40;
     int maxNumberOfHeroes = 6;
     int maxNumberOfEachCard = 3;
+    int heroDef = 2;
+    std::string firstRegion = "";
+    std::string secondRegion = "";
 }
 
-int DeckBuilder::checkNumberOfCard(Card card) 
-{
+int DeckBuilder::checkNumberOfCard(Card card) {
     int number = 0;
-    for (Card c : deck.getCardsAsVector()) 
-    {
-        if (c == card) 
-        {
+    for (Card c : deck.getCardsAsVector()) {
+        if (c == card) {
             number++;
         }
     }
@@ -24,38 +25,57 @@ void DeckBuilder::addCard(Card &cardToAdd)
 {
     int dl = deck.length();
     if (dl < maxNumberOfCards && checkNumberOfCard(cardToAdd) < maxNumberOfEachCard) {
-        if (firstRegion == "") 
-        {
+        if (firstRegion == "") {
             firstRegion = cardToAdd.getRegion();
-        } 
-        else if (secondRegion == "") 
-        {
+        } else if (secondRegion == "") {
             secondRegion = cardToAdd.getRegion();
-        } 
-        else if (!(firstRegion == cardToAdd.getRegion() || secondRegion == cardToAdd.getRegion())) 
-        {
+        } else if (!(firstRegion == cardToAdd.getRegion() || secondRegion == cardToAdd.getRegion())) {
             throw new std::exception;                                       //TODO: change exception
             return;
         }
-
-        if (cardToAdd.getType() == "hero" && maxNumberOfHeroes > 0) 
-        {       //TODO: correct type
-            maxNumberOfHeroes--;
+        if (cardToAdd.getType() == "hero" && maxNumberOfHeroes > 0) {       //TODO: correct type
+            maxNumberOfHeroes--; //idk if this is correct i think it should be deck.numberOfHeroes++;
             deck.addCard(cardToAdd);
-        } 
-        else 
-        {
+        } else {
             deck.addCard(cardToAdd);
         }
-    } 
-    else 
-    {
+    } else {
         throw new std::exception;                                            //TODO: change exception
     }
 }
 
-void removeCard(Card &cardToRemove)
+void DeckBuilder::removeCard(Card &cardToRemove)
 {
+    int dl = deck.length();
+    for (int i = 0; i < dl; i++){
+        //if (deck[i].getCardCode() == cardToRemove.getCardCode()) //mistake (i think) cant compare strings that way
+        if (deck[i].getCardCode().compare(cardToRemove.getCardCode()) == 0){
+            string regioncd = deck[i].region;
+            if (deck[i].getType().compare("hero")){
+                maxNumberOfHeroes++; //idk if this is correct should be deck.numberOfHeroes--;
+            }
+            deck.erase(i);
+            maxNumberOfCards++; //idk if this is correct should be deck.numberOfCards--;
+            int counter = 0;
+            for (int j = 0; j < dl; j++){
+                if(deck[j].getRegion.compare(regioncd) == 0){
+                    break;
+                } else {
+                    counter++;
+                }
+            }
+            if (counter == dl){
+                if (firstRegion.compare(regioncd) == 0){
+                    firstRegion = "";
+                    return;
+                } else if(secondRegion.compare(regioncd) == 0){
+                    secondRegion = "";
+                    return;
+                }
+            } 
+            return;
+        }
+    }
 
 }
 
