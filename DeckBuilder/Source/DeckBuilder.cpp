0115110eb13c83e87830f732e2863e820b19cc0d
Wiktor Szewczyk <wiktorszewczyk21@interia.pl>
2021-04-13 20:45:35 +0200

    Basic refactor

--- DeckBuilder/Source/DeckBuilder.cpp
+++ DeckBuilder/Source/DeckBuilder.cpp
@@ -11,10 +11,13 @@ DeckBuilder::DeckBuilder()
     std::string secondRegion = "";
 }
 
-int DeckBuilder::checkNumberOfCard(Card card) {
+int DeckBuilder::checkNumberOfCard(Card card) 
+{
     int number = 0;
-    for (Card c : deck.getCardsAsVector()) {
-        if (c == card) {
+    for (Card c : deck.getCardsAsVector()) 
+    {
+        if (c == card) 
+        {
             number++;
         }
     }
@@ -24,22 +27,34 @@ int DeckBuilder::checkNumberOfCard(Card card) {
 void DeckBuilder::addCard(Card &cardToAdd)
 {
     int dl = deck.length();
-    if (dl < maxNumberOfCards && checkNumberOfCard(cardToAdd) < maxNumberOfEachCard) {
-        if (firstRegion == "") {
+    if (dl < maxNumberOfCards && checkNumberOfCard(cardToAdd) < maxNumberOfEachCard) 
+    {
+        if (firstRegion == "") 
+        {
             firstRegion = cardToAdd.getRegion();
-        } else if (secondRegion == "") {
+        } 
+        else if (secondRegion == "") 
+        {
             secondRegion = cardToAdd.getRegion();
-        } else if (!(firstRegion == cardToAdd.getRegion() || secondRegion == cardToAdd.getRegion())) {
+        } 
+        else if (!(firstRegion == cardToAdd.getRegion() || secondRegion == cardToAdd.getRegion())) 
+        {
             throw new std::exception;                                       //TODO: change exception
             return;
         }
-        if (cardToAdd.getType() == "hero" && maxNumberOfHeroes > 0) {       //TODO: correct type
+
+        if (cardToAdd.getType() == "hero" && maxNumberOfHeroes > 0) 
+        {       //TODO: correct type
             maxNumberOfHeroes--; //idk if this is correct i think it should be deck.numberOfHeroes++;
             deck.addCard(cardToAdd);
-        } else {
+        } 
+        else 
+        {
             deck.addCard(cardToAdd);
         }
-    } else {
+    } 
+    else 
+    {
         throw new std::exception;                                            //TODO: change exception
     }
 }
@@ -47,29 +62,41 @@ void DeckBuilder::addCard(Card &cardToAdd)
 void DeckBuilder::removeCard(Card &cardToRemove)
 {
     int dl = deck.length();
-    for (int i = 0; i < dl; i++){
+    for (int i = 0; i < dl; i++)
+    {
         //if (deck[i].getCardCode() == cardToRemove.getCardCode()) //mistake (i think) cant compare strings that way
-        if (deck[i].getCardCode().compare(cardToRemove.getCardCode()) == 0){
+        if (Deck::deck.at(i).getCardCode().compare(cardToRemove.getCardCode()) == 0)
+        {
             string regioncd = deck[i].region;
-            if (deck[i].getType().compare("hero")){
+            if (getCardsAsVector.at(i).getType().compare("hero"))
+            {
                 maxNumberOfHeroes++; //idk if this is correct should be deck.numberOfHeroes--;
             }
             deck.erase(i);
             maxNumberOfCards++; //idk if this is correct should be deck.numberOfCards--;
             int counter = 0;
-            for (int j = 0; j < dl; j++){
-                if(deck[j].getRegion.compare(regioncd) == 0){
+            for (int j = 0; j < dl; j++)
+            {
+                if(deck[j].getRegion.compare(regioncd) == 0)
+                {
                     break;
-                } else {
+                } 
+                else 
+                {
                     counter++;
                 }
             }
+
             dl = deck.length();
-            if (counter == dl){
-                if (firstRegion.compare(regioncd) == 0){
+            if (counter == dl)
+            {
+                if (firstRegion.compare(regioncd) == 0)
+                {
                     firstRegion = "";
                     return;
-                } else if(secondRegion.compare(regioncd) == 0){
+                } 
+                else if (secondRegion.compare(regioncd) == 0)
+                {
                     secondRegion = "";
                     return;
                 }
@@ -77,7 +104,6 @@ void DeckBuilder::removeCard(Card &cardToRemove)
             return;
         }
     }
-
 }
 
 DeckBuilder::~DeckBuilder() {}
