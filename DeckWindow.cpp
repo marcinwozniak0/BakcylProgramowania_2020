cfb292f3d89c689097ee20752f03e699ca1fc272
Jacek Jarosz <jacekjarosz55@gmail.com>
2021-05-14 13:32:58 +0200

    Chart Improvements, basics for getting card type data for the chart.
    For now the chart doesnt work yet, because we couldn't retrieve the type
    in any way.
    Also removed that accidental database.sql, which was an empty file.

--- DeckWindow.cpp
+++ DeckWindow.cpp
@@ -1,6 +1,5 @@
 #include "DeckWindow.h"
 #include "ui_deckwindow.h"
-
 const size_t deckWindowWidth = 890;
 const size_t deckWindowHeight = 525;
 
@@ -19,31 +18,82 @@ DeckWindow::DeckWindow(DeckBuilder* deck, QRect geometry, QWidget *parent) :
 }
 void DeckWindow::CreateTypesChart()
 {
+    int units = 0;
+    int skills = 0;
+    int spells = 0;
+    int landmarks = 0;
+ /* Pierwszy pomysł na zdobycie ilości typów. Niestety wszystkie typy kart na ten moment nazywają się "type" :p
+    for(const auto& card : deck_->getDeck().getCardsAsVector())
+    {
+        switch(card.type[0])
+        {
+            case 'J': // "Jednostka"
+                units++;
+                break;
+
+            case 'U': // "Umiejętność"
+                skills++;
+                break;
+
+            case 'Z': // "Zaklęcie"
+                spells++;
+                break;
+
+            case 'L': // "Lokacja"
+                landmarks++;
+                break;
+        }
+    }
+
+*/
 
     series = new QPieSeries();
     series->setVisible(false);
-    series->append(" ",0);
-    series->append("Bohaterowie",40);
-    series->append("Specjalne",40);
-    series->append("Spell",69);
+    series->setPieSize(0.5);
+    series->setHoleSize(0.2);
 
-
-    for (int i = 1; i < 4; i++) {
-        QPieSlice *slice = series->slices().at(i);
+int piesize = 0;
+if(units > 0)
+{
+    series->append(QString::fromStdString("Units: "+std::to_string(units)),units);
+    piesize++;
+}
+if(skills > 0)
+{
+    series->append(QString::fromStdString("Skills: "+std::to_string(skills)),skills);
+    piesize++;
+}
+if(spells > 0)
+{
+    series->append(QString::fromStdString("Spells: "+std::to_string(spells)),spells);
+    piesize++;
+}
+if(landmarks > 0)
+{
+    series->append(QString::fromStdString("Landmarks: "+std::to_string(landmarks)),landmarks);
+    piesize++;
+}
+for (int i = 0; i < piesize; i++) {
+        QPieSlice* slice = series->slices().at(i);
         slice->setLabelVisible(true);
-        slice->setPen(QPen(Qt::black));
-        slice->setBrush(QColor((i*100)%255,(i*100)%255,(i*100)%255));
-        slice->setLabelColor(Qt::white);
+        slice->setBorderWidth(0);
+        slice->setBorderColor(Qt::transparent);
+        slice->setBrush(QColor(0,100,((i+130)*70)%256));
+        slice->setLabelColor(Qt::gray);
     }
+    //series->slices().at(selected)->setExploded(true);
+
+
     chart = new QChart();
     chart->addSeries(series);
     chart->setBackgroundVisible(false);
     chartview = new QChartView(chart, ui->Statystyki);
-    chartview->setGeometry(300, 0, 350, 200);
+    chartview->setGeometry(270, -50, 400, 300);
 
 }
 void DeckWindow::CheckDeckStats()
 {
+
     ui->Statystyki->findChild<QLabel*>("Region1")->setText(QString::fromStdString(deck_->firstRegion.name));
     ui->Statystyki->findChild<QLabel*>("Region2")->setText(QString::fromStdString(deck_->secondRegion.name));
     double healthsum = 0;
